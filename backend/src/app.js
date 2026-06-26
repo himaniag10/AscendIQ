555093be5d90a39ca0fd214c064881ead60234ef
himaniag10 <himaniagarwal1006@gmail.com>
2026-06-26 09:53:53 +0000

    bug resolve

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,8 @@ app.use(helmet());
 // Enable CORS
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://ascend-mock-interview.vercel.app"
 ];
 
 app.use(cors({
